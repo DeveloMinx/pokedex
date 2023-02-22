de0d5081be66770ea245b556c4bb2f43a2eeae78
MinaCode24 <anderson.mina.ix98@gmail.com>
2023-02-22 15:43:26 -0500

    Se crearon los componentes y servisios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { ShowComponent } from './components/show/show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    DetailComponent,
+    ShowComponent
   ],
   imports: [
     BrowserModule,
